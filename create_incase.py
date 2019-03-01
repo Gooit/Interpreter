e4e7bedf7b688c8382b9216947df688477400832
chengzhengwu <chengzhengwu@aliyun.com>
2019-03-01 14:20:17 +0800

    增加新样例

--- create_incase.py
+++ create_incase.py
@@ -1,11 +1,15 @@
-
-
-
+import os
 import random
+
+
+problem_id = 1
 for i in range(1000):
     a = random.randint(-1000000, 1000000)
     b = random.randint(-1000000, 1000000)
     data = "%s %s" % (a, b)
-    with ('cases/1/in/'+i+".in", 'w') as f:
+    path = 'cases/%s/in/' % problem_id
+    if not os.path.exists(path):
+	    os.makedirs(path) 
+    with open(path + "%s.in" % i, 'w') as f:
         f.write(data)
     
---
+++ create_outcase.py
@@ -0,0 +1,8 @@
+
+
+import os
+
+problem_id = 1
+path = "cases/%s/in" % problem_id
+for i in range(1000):
+	with open('
